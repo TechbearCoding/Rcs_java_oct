b515d4b90e0cd5442077486d185573251b119834
Techbear <21012644+TechbearCoding@users.noreply.github.com>
2023-11-07 20:53:55 +0200

    Samples

---
+++ Day1_IO/src/main/java/org/example/Main.java
@@ -0,0 +1,32 @@
+package org.example;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+//        System.out.println("Hello world!");
+//        System.out.println("Mani sauc Mārtiņš");
+//        System.out.println("1+2");
+//        System.out.println(1+2);
+//        System.out.println("aaaa " + "bbb");
+
+//        String a = "aaaaa"; //simbolu virkne
+//        System.out.println(a);
+//        a = "abc";
+//        System.out.println(a + "bbb");
+//
+//        int b = 1; //Vesels skaitlis
+//        b = 2+4;
+//        System.out.println(b - 9);
+//
+//        double c = 4.2;
+//        boolean d = true;
+//        char e = 'a';
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Ievadiet vārdu!");
+        String ievade = sc.nextLine();
+        System.out.println(ievade);
+    }
+}
