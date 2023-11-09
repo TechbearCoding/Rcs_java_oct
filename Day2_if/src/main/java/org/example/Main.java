f940a71fa110fc0dcd56d4b537223e3dd3d3e23c
Techbear <21012644+TechbearCoding@users.noreply.github.com>
2023-11-09 19:19:15 +0200

    Day samples

---
+++ Day2_if/src/main/java/org/example/Main.java
@@ -0,0 +1,41 @@
+package org.example;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        task1();
+    }
+
+    public static void task1(){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Ievadiet skaitli!");
+        int a = sc.nextInt();
+
+        System.out.println("Ievadiet 2. skaitli!");
+        int b = sc.nextInt();
+
+        System.out.println(a + b);
+
+    }
+
+    public static void sample2(){
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Ievadiet skaitli!");
+        int a = sc.nextInt();
+
+        System.out.println(a);
+    }
+
+    public static void sample1(){
+        System.out.println("Ievadi savu vārdu!");
+
+        Scanner sc = new Scanner(System.in);
+
+        String input = sc.nextLine();
+
+        System.out.println("Sveiki, " + input);
+    }
+}
+
