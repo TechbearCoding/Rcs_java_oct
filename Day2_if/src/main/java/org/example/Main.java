28e166102981b44a93c784b0003af2ea6e673546
Techbear <21012644+TechbearCoding@users.noreply.github.com>
2023-11-09 21:26:30 +0200

    and or samles

--- Day2_if/src/main/java/org/example/Main.java
+++ Day2_if/src/main/java/org/example/Main.java
@@ -4,7 +4,41 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        sample3();
+        sample4();
+    }
+
+    public static void sample4(){
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Ievadiet vārdu!");
+        String b = sc.nextLine();
+
+        int len = b.length(); //simbolu skaits
+
+        if(b.equals("bbb") ){
+            System.out.println("ir vienāds");
+        }else if(b.length() == 3){
+            System.out.println("ir vienāds");
+        }
+
+
+        if(b.equals("bbb") || b.length() == 3){
+            System.out.println("ir vienāds");
+        }
+        // || vai - jaizpildas vienam no nosacijumiem
+        // && un - jaizpildas abiem diviem
+    }
+
+    static void task2(){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Ievadiet skaitli!");
+        int a = sc.nextInt();
+
+        if(a > 5){
+            System.out.println("###");
+        }else{
+            System.out.println("####");
+        }
     }
 
     public static void sample3(){
@@ -45,7 +79,6 @@ public class Main {
         int b = sc.nextInt();
 
         System.out.println(a + b);
-
     }
 
     public static void sample2(){
