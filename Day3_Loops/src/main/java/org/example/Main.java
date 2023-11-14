688667779bcf5a574486c972784c7c5bfa90eede
Techbear <21012644+TechbearCoding@users.noreply.github.com>
2023-11-14 20:30:04 +0200

    loop in if sample

--- Day3_Loops/src/main/java/org/example/Main.java
+++ Day3_Loops/src/main/java/org/example/Main.java
@@ -4,15 +4,59 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        sample2();
-        //task1();
+//        String a = "aaa";
+//        int b = a.length();
+
+        sample4();
+//        task2();
+    }
+
+    public static void sample4(){
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Lūdzu ievadiet skaitli!");
+        int number = sc.nextInt();
+
+        if(number == 5){
+            for(int i = 0; i < 5; i++){
+                System.out.println("hello");
+            }
+        }else{
+            System.out.println("Kļūda");
+        }
+    }
+
+    public static void sample3(){
+        Scanner sc = new Scanner(System.in);
+
+        for(int i = 0; i < 5; i++){
+            System.out.println("Lūdzu ievadiet skaitli!");
+            int number = sc.nextInt();
+
+            if(number == 0){
+                System.out.println("Kļūda");
+            }else if(number == 4){
+                System.out.println("Hello world!");
+            }else{
+                System.out.println("Hello world!2");
+            }
+        }
+    }
+
+    public static void task2(){
+        Scanner sc = new Scanner(System.in);
+
+        int len = 0;
+        for(int i = 0; i < 5; i++){
+            System.out.println("Ievadīt simbolu virkni!");
+            String input = sc.nextLine();
+            len = len + input.length();
+        }
+        System.out.println("Kopējais garums ir " + len);
     }
 
     public static void sample2(){
         Scanner sc = new Scanner(System.in);
-
-        String a = "aaa";
-        int b = a.length();
 
         int sum = 0;
         for(int i = 0; i < 3; i++){
