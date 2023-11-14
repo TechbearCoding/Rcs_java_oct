412d046718b4d93152e27a2b5026699d416d0cf8
Techbear <21012644+TechbearCoding@users.noreply.github.com>
2023-11-14 19:37:03 +0200

    sum sample

--- Day3_Loops/src/main/java/org/example/Main.java
+++ Day3_Loops/src/main/java/org/example/Main.java
@@ -1,8 +1,40 @@
 package org.example;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        sample1();
+        sample2();
+        //task1();
+    }
+
+    public static void sample2(){
+        Scanner sc = new Scanner(System.in);
+
+        String a = "aaa";
+        int b = a.length();
+
+        int sum = 0;
+        for(int i = 0; i < 3; i++){
+            System.out.println("Lūdzu ievadiet skaitli!");
+            int number = sc.nextInt();
+            sum = sum + number;
+            System.out.println(number);
+            System.out.println("-------------");
+
+        }
+        System.out.println("Summa ir " + sum);
+
+    }
+
+    public static void task1(){
+        Scanner sc = new Scanner(System.in);
+
+        for(int i = 0; i < 7; i++){
+            System.out.println("Lūdzu ievadiet skaitli!");
+            int number = sc.nextInt();
+            System.out.println(number + 5);
+        }
     }
 
     public static void sample1(){
