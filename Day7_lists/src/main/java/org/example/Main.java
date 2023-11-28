ebc75d0d8d1353b47fb37196402c0984932ab6cf
Techbear <21012644+TechbearCoding@users.noreply.github.com>
2023-11-28 19:47:46 +0200

    samples lists

--- Day7_lists/src/main/java/org/example/Main.java
+++ Day7_lists/src/main/java/org/example/Main.java
@@ -2,8 +2,9 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
-        int[] a = {2, 5, 6, 8};
-        double[] b = {1.1, 2, 5, 7, 8.9};
-        System.out.println(Tasks.task1(b));
+//        int[] a = {2, 5, 6, 8};
+//        double[] b = {1.1, 2, 5, 7, 8.9};
+//        System.out.println(Tasks.task1(b));
+        Samples.run();
     }
 }
---
+++ Day7_lists/src/main/java/org/example/Samples.java
@@ -0,0 +1,27 @@
+package org.example;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Samples {
+
+    public static void run() {
+        sample1();
+    }
+
+    private static void sample1() {
+        List<Integer> numberList = new ArrayList<>();
+        numberList.add(3);
+        numberList.add(7);
+        numberList.add(5);
+
+        System.out.println(numberList.get(0));
+
+        for(int i = 0; i < numberList.size(); i++){
+            System.out.println(numberList.get(i));
+        }
+
+
+        numberList.remove(0);
+    }
+}
