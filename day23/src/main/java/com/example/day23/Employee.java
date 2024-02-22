031f10c355fef28663f09e36d8254c2ad6dc2333
Martins Leitarts <21012644+TechbearCoding@users.noreply.github.com>
2024-02-22 20:39:44 +0200

    api + swagger

---
+++ day23/src/main/java/com/example/day23/Employee.java
@@ -0,0 +1,15 @@
+package com.example.day23;
+
+public class Employee {
+    public int id;
+    public String firstName;
+    public String lastName;
+    public int years;
+
+    public Employee(int id, String firstName, String lastName, int years){
+        this.id = id;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.years = years;
+    }
+}
