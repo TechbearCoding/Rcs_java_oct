3f63f426be2fed86be1435aa0dfb23543a21400b
Martins Leitarts <martins.leitarts@kleintech.net>
2024-02-27 19:19:56 +0200

    day23 task

--- day23/src/main/java/com/example/day23/DemoService.java
+++ day23/src/main/java/com/example/day23/DemoService.java
@@ -4,17 +4,14 @@ import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.sql.Connection;
 
 @Service
 public class DemoService {
 
     public List<Employee> getEmployees(){
-        List<Employee> lst = new ArrayList<>();
-
-        lst.add(new Employee(1, "Janis", "Berzins", 1));
-        lst.add(new Employee(2, "Edgars", "Ozols", 4));
-        lst.add(new Employee(3, "Juris", "Klava", 3));
-
+        Connection conn = Connect.connect();
+        List<Employee> lst = Connect.getEmployees(conn);
         return lst;
     }
 
